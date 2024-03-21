de90ba2204ccf04061cdd4771093e625fc01178d
Eduardo Araujo <eduardo1996araujo@gmail.com>
2024-03-21 19:17:07 -0300

    Configurando o mysql pelo docker-compose e o application properties

---
+++ src/main/java/com/eduardo/agregadorinvestimentos/AgregadorinvestimentosApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.agregadorinvestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadorinvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregadorinvestimentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardo/agregadorinvestimentos/AgregadorinvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.agregadorinvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadorinvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
